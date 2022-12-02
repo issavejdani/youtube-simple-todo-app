e76cf635e05fc7fba3899b45be917899946d209c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-12-02 21:13:26 +0330

    sprint 02 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Header from "./components/ui/Header.component";
+import Footer from "./components/ui/Footer.component";
 
 function App() {
   const [list, setList] = useState([]);
@@ -16,33 +18,46 @@ function App() {
     setValue("");
   };
 
+  const handleClearAll = () => {
+    setList([]);
+  };
+
   return (
-    <div className="container">
-      {/* heading */}
-      <h2>Youtube Simple Todo App</h2>
+    <>
+      <Header />
+      <div className="container">
+        {/* heading */}
+        <h2>
+          Youtube Simple Todo App{" "}
+          <button className="ml-6 btn-clear-all" onClick={handleClearAll}>
+            Clear All
+          </button>
+        </h2>
 
-      {/* create new todo */}
-      <div className="create-section">
-        <label>Todo</label>
-        <input type="text" className="input" value={value} onChange={(e) => setValue(e.target.value)} />
-        <button className="button" onClick={clickHandler}>
-          Save
-        </button>
-      </div>
+        {/* create new todo */}
+        <div className="create-section">
+          <label>Todo</label>
+          <input type="text" className="input" value={value} onChange={(e) => setValue(e.target.value)} />
+          <button className="button" onClick={clickHandler}>
+            Save
+          </button>
+        </div>
 
-      {/* show our todo list */}
-      <div className="todo-list">
-        {list.length > 0 ? (
-          list.map((item, index) => (
-            <div key={index} className="todo-item">
-              {item}
-            </div>
-          ))
-        ) : (
-          <h1>No Todo !!!!</h1>
-        )}
+        {/* show our todo list */}
+        <div className="todo-list">
+          {list.length > 0 ? (
+            list.map((item, index) => (
+              <div key={index} className="todo-item">
+                {item}
+              </div>
+            ))
+          ) : (
+            <h1>No Todo !!!!</h1>
+          )}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
